937c229a23fc783147d0fca92025f39238651fe8
Shaobo Wang <wangshaobonju@gmail.com>
2013-10-13 19:18:59 -0400

    Read string from index.html

--- web.js
+++ web.js
@@ -1,9 +1,12 @@
 var express = require('express');
+var fs = require('fs');
+var data = fs.readFileSync("index.html","utf-8");
 var app = express();
 app.use(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World on New Ubuntu!');
+     
+  response.send(data);
 });
 
 var port = process.env.PORT || 5000;
